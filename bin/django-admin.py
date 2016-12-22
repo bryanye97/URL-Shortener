42949d919600f91695c24165c534ff68a1b8f76d
Bryan <bryanye97@gmail.com>
2016-12-22 15:22:32 +1100

    First commit

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/bryanye/Developer/Languages/Python/Django/django110/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
